66a70c5037f8be756fcef3fb9fda7ca2ec697895
Hamzat <hamzy2000@gmail.com>
2015-04-17 07:08:28 -0700

    Added additional functionalities

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 //global variables
 var obj = {}; // this holds the input values
-var inputYArrayTwo;
-//windows onload function
+var arraySplittedYTwo;
+//windows onload function. This contains every other functions
 function init(){
 
-	var chartType = document.getElementById('chartType').value;
+	var chartType = document.getElementById('chartType').value;//get type of chart
 	var select = document.getElementById('chartType');
 
+	//event for when select box is changed
 	select.addEventListener('change', function(e){
 		chartType = select.value;
 	});
-
-	
 
 	//get input values when submit button is clicked and generate chart when chartGenerate button is clicked
 	document.getElementById('formSubmit').addEventListener('click', getInputValues);
@@ -22,176 +21,178 @@ function init(){
 	var c = graph.getContext('2d');
 	document.getElementById('chartGenerator').disabled = true;
 
-//Getting values from input boxes
+	//Getting values from input boxes
 	function getInputValues(e) {
 	e.preventDefault(); //this prevents search button default reload onclick
-  var inputForX = document.getElementById('xAxisInput').value;
-	var inputForY = document.getElementById('yAxisInput').value;
+  var initialX = document.getElementById('xAxisInput').value; //initial x values from textbox
+	var initialY = document.getElementById('yAxisInput').value; //initial y values from textbox
 
 	//Testing for non-empty input values and process values to return for line chart
 	if(chartType === 'Line Chart'){
-		if(inputForX == '' || inputForY == ''){
+		if(initialX == '' || initialY == ''){
 			alert("Please, fill in your x and y labels/values");
 		}
+		//the else part
 		else{
-			var inputXArray = inputForX.split(',');
-			var inputYArray = inputForY.split(',');
-			var data = [inputXArray,inputYArray];
-    	var x_labels = data[0];
-    	var y_values = data[1];
+			var arraySplittedX = initialX.split(','); //intial x splitted into an array
+			var arraySplittedY = initialY.split(','); //initial y splitted into an array
+			var data = [arraySplittedX,arraySplittedY]; //this holds the two arrays
+    	var x_labels = data[0]; //x_labels represent first array
+    	var y_values = data[1]; //y_labels represent second array
 
-    	obj["inputValues"] = new Array();
+    	obj["inputValues"] = new Array();//creating an instance obj of array
+      //a loop to get objects of array of object
     	for (var i = 0; i < x_labels.length; i++) {
     		var item = {};
     		item["X"] = x_labels[i];
-    		item["Y"] = Number(y_values[i]);
-    		obj.inputValues.push(item);
+    		item["Y"] = Number(y_values[i]); //convert y_values to number
+    		obj.inputValues.push(item); //add each element into array obj
     	};
 
-    	document.getElementById('chartGenerator').disabled = false;
-  		return obj;
+    	document.getElementById('chartGenerator').disabled = false; //this enables chart generation button
+  		return obj; // object obj is return by this function
 		}
 	}
 
 	//Testing for non-empty input values and process values to return for pie chart
 	if (chartType === 'Pie Chart') {
-		if(inputForX === '' || inputForY === ''){
+		if(initialX === '' || initialY === ''){
 			alert("Please, fill in your x and y labels/values");
 		}
+    //else part
 		else{
-			var inputXArray = inputForX.split(',');
-			var inputYArray = inputForY.split(',');
-			inputYArrayTwo = [];
-			for(var x in inputYArray){
-				inputYArrayTwo.push(Number(inputYArray[x]));
+			var arraySplittedX = initialX.split(',');
+			var arraySplittedY = initialY.split(',');
+			arraySplittedYTwo = [];
+			for(var x in arraySplittedY){
+				arraySplittedYTwo.push(Number(arraySplittedY[x]));
 			};
 
-			console.log("Y array " + inputYArrayTwo);
-			document.getElementById('chartGenerator').disabled = false;
-			
+			document.getElementById('chartGenerator').disabled = false; //enable the char generation button
 		}
 	}
 }
 
-//Draw line chart function
-function drawLine() {
+  //Draw line chart function
+  function drawLine() {
 
-	var xPadding = 30; //padding for x axis
-	var yPadding = 30; //padding for y axis
+  	var xPadding = 30; //padding for x axis and labels
+  	var yPadding = 30; //padding for y axis and labels
 
-	function getMaxY() {		
-  	var maxim = 0;
-     
-  for(var i = 0; i < obj.inputValues.length; i++) {
-    if(obj.inputValues[i].Y > maxim) {
-      maxim = obj.inputValues[i].Y;
+    //
+  	function getMaxY() {		
+    	var maxim = 0;
+       
+    for(var i = 0; i < obj.inputValues.length; i++) {
+      if(obj.inputValues[i].Y > maxim) {
+        maxim = obj.inputValues[i].Y;
+      }
     }
+       
+     maxim += 10 - maxim % 10;
+     return maxim;
+  	}
+
+  	function getXPixel(val) {
+    	return ((graph.width - xPadding) / obj.inputValues.length) * val + (xPadding * 1.5);
+  	}
+   
+  	function getYPixel(val) {
+      return graph.height - (((graph.height - yPadding) / getMaxY()) * val) - yPadding;
+  	}
+
+  		c.clearRect(0, 0, graph.width, graph.height);
+  		c.lineWidth = 2;
+  		c.strokeStyle = '#333';
+  		c.font = '10pt sans-serif';
+  		c.textAlign = "center";
+  		c.beginPath();
+  		c.moveTo(xPadding, 0);
+  		c.lineTo(xPadding, graph.height - yPadding);
+  		c.lineTo(graph.width, graph.height - yPadding);
+  		c.stroke();
+
+  		for(var i = 0; i < obj.inputValues.length; i++) {
+  	    c.fillText(obj.inputValues[i].X, getXPixel(i), graph.height - yPadding + 20);
+  		}
+
+  		c.textAlign = "right"
+  		c.textBaseline = "middle";
+  	 
+  		for(var i = 0; i < getMaxY(); i += 10) {
+  	    c.fillText(i, xPadding - 10, getYPixel(i));
+  		}
+
+  		c.strokeStyle = '#f00';
+  		c.beginPath();
+  		c.moveTo(getXPixel(0), getYPixel(obj.inputValues[0].Y));
+  	 
+  		for(var i = 1; i < obj.inputValues.length; i ++) {
+  	    c.lineTo(getXPixel(i), getYPixel(obj.inputValues[i].Y));
+  		}
+  		c.stroke();
+
+  		c.fillStyle = '#333';
+  	 
+  		for(var i = 0; i < obj.inputValues.length; i ++) {  
+  	    c.beginPath();
+  	    c.arc(getXPixel(i), getYPixel(obj.inputValues[i].Y), 4, 0, Math.PI * 2, true);
+  	    c.fill();
+  		}
+  	document.getElementById('chartGenerator').disabled = true;
+}
+
+//Draw pie chart function getTotal of input Y
+  function getTotal(){
+  		var myTotal = 0;
+  		console.log("getTotal array " + arraySplittedYTwo);
+  		for (var j = 0; j < arraySplittedYTwo.length; j++) {
+  			myTotal += (typeof arraySplittedYTwo[j] == 'number') ? arraySplittedYTwo[j] : 0;
+  		}
+  	return myTotal;
   }
-     
-   maxim += 10 - maxim % 10;
-   return maxim;
-	}
 
-	function getXPixel(val) {
-  	return ((graph.width - xPadding) / obj.inputValues.length) * val + (xPadding * 1.5);
-	}
- 
-	function getYPixel(val) {
-    return graph.height - (((graph.height - yPadding) / getMaxY()) * val) - yPadding;
-	}
+  //actual function to draw pie chart
+  function drawPie() {
+  	var lastend = 0; 
+  	var myTotal = getTotal();
+  	myData = arraySplittedYTwo;
+  	c.clearRect(0, 0, graph.width, graph.height); //set canvas to empty on start
+  	var myColor = ["#ECD078","#D95B43","#C02942","#542437","#53777A"]; //color fills for the data
 
-		c.clearRect(0, 0, graph.width, graph.height);
-		c.lineWidth = 2;
-		c.strokeStyle = '#333';
-		c.font = '10pt sans-serif';
-		c.textAlign = "center";
-		c.beginPath();
-		c.moveTo(xPadding, 0);
-		c.lineTo(xPadding, graph.height - yPadding);
-		c.lineTo(graph.width, graph.height - yPadding);
-		c.stroke();
+  	for (var i = 0; i < myData.length; i++) {
 
-		for(var i = 0; i < obj.inputValues.length; i++) {
-	    c.fillText(obj.inputValues[i].X, getXPixel(i), graph.height - yPadding + 20);
-		}
+  		c.fillStyle = myColor[i];
+  		c.beginPath();
+  		c.moveTo(200,150);
+  		c.arc(200,150,150,lastend,lastend+(Math.PI*2*(myData[i]/myTotal)),false);
+  		
+  		c.lineTo(200,150);
+  		c.fill();
+  		lastend += Math.PI*2*(myData[i]/myTotal);
+  	}
 
-		c.textAlign = "right"
-		c.textBaseline = "middle";
-	 
-		for(var i = 0; i < getMaxY(); i += 10) {
-	    c.fillText(i, xPadding - 10, getYPixel(i));
-		}
+  	document.getElementById('chartGenerator').disabled = true; //disabled chartGenerator button
+  }
 
-		c.strokeStyle = '#f00';
-		c.beginPath();
-		c.moveTo(getXPixel(0), getYPixel(obj.inputValues[0].Y));
-	 
-		for(var i = 1; i < obj.inputValues.length; i ++) {
-	    c.lineTo(getXPixel(i), getYPixel(obj.inputValues[i].Y));
-		}
-		c.stroke();
+  //function to be passed to the chartGenerator button due to a condiyion
+  function drawChart(){
+  	console.log(chartType);
+  	if(chartType ==='Line Chart'){
+  		drawLine();
+  		
+  	} else if(chartType === 'Pie Chart'){
+  		console.log('drawPie');
+  		drawPie();
+  	}
+  }
 
-		c.fillStyle = '#333';
-	 
-		for(var i = 0; i < obj.inputValues.length; i ++) {  
-	    c.beginPath();
-	    c.arc(getXPixel(i), getYPixel(obj.inputValues[i].Y), 4, 0, Math.PI * 2, true);
-	    c.fill();
-		}
-	document.getElementById('chartGenerator').disabled = true;
-}
-
-//Draw pie chart function
-
-function getTotal(){
-		var myTotal = 0;
-		console.log("getTotal array " + inputYArrayTwo);
-		for (var j = 0; j < inputYArrayTwo.length; j++) {
-			myTotal += (typeof inputYArrayTwo[j] == 'number') ? inputYArrayTwo[j] : 0;
-		}
-	return myTotal;
-}
-
-function drawPie() {
-	var lastend = 0;
-	console.log("drawLine Y array " + inputYArrayTwo);
-	var myTotal = getTotal();
-	myData = inputYArrayTwo;
-	c.clearRect(0, 0, graph.width, graph.height);
-	var myColor = ["#ECD078","#D95B43","#C02942","#542437","#53777A"];
-
-	for (var i = 0; i < myData.length; i++) {
-
-		c.fillStyle = myColor[i];
-		c.beginPath();
-		c.moveTo(200,150);
-		c.arc(200,150,150,lastend,lastend+(Math.PI*2*(myData[i]/myTotal)),false);
-		
-		c.lineTo(200,150);
-		c.fill();
-		lastend += Math.PI*2*(myData[i]/myTotal);
-	}
-
-	document.getElementById('chartGenerator').disabled = true;
-}
-
-function drawChart(){
-	console.log(chartType);
-	if(chartType ==='Line Chart'){
-		drawLine();
-		
-	} else if(chartType === 'Pie Chart'){
-		console.log('drawPie');
-		drawPie();
-	}
-}
-
-//clear canvas button
-document.getElementById('clearCanvas').addEventListener('click', function() {
-  c.clearRect(0, 0, graph.width, graph.height);
-}, false);
+  //clear canvas button
+  document.getElementById('clearCanvas').addEventListener('click', function() {
+    c.clearRect(0, 0, graph.width, graph.height);
+  }, false);
 
 }
 
-window.onload = init;
+window.onload = init; //load the init function automatically
 
